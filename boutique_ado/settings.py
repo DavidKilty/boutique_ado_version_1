a2db63c5ab9d59651f500536048d8929c059a7ca
David Kilty <davidbkilty@gmail.com>
2024-09-19 16:27:29 +0000

    set up all auth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--z2)ert&=gu+7pd=f592e6fh=^mehr7w+pgl*6kfjti&9(yx5j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = [
 
@@ -89,6 +89,15 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 1
 
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/success'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
