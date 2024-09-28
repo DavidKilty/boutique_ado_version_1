5cc2f1c1bad8dd89ac7aaaac56f5d9418fcd51ad
David Kilty <davidbkilty@gmail.com>
2024-09-28 12:47:06 +0000

    added product search function

--- home/views.py
+++ home/views.py
@@ -1,17 +1,8 @@
-# home/views.py
-
 from django.shortcuts import render
 
-# Home page view
-def index(request):
-    """
-    A view to return the index page
-    """
-    return render(request, 'home/index.html')
+# Create your views here.
 
-# Profile page view
-def profile(request):
-    """
-    A view to return the user's profile page
-    """
-    return render(request, 'profile.html')
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/index.html')
